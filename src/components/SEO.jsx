b1809796a0d396dfebdfcf7afa8d691bd195d2ad
noino486 <ofmn360@gmail.com>
2025-10-17 20:24:07 +0200

    ajoute des rediction direct

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useAffiliate } from '../context/AffiliateContext'
 
 const SEO = ({ 
   title = 'Evo E-com | Liste fournisseurs internationaux',
@@ -8,6 +9,20 @@ const SEO = ({
   ogImage = '/og-image.jpg',
   ogUrl = 'https://www.evoecom.com/'
 }) => {
+  const { getCurrentAffiliateCode } = useAffiliate()
+  const affiliateCode = getCurrentAffiliateCode()
+  
+  // Construire l'URL avec le code d'affiliation si présent
+  const buildUrl = (baseUrl) => {
+    if (affiliateCode) {
+      const url = new URL(baseUrl)
+      url.searchParams.set('AF', affiliateCode)
+      return url.toString()
+    }
+    return baseUrl
+  }
+
+  const finalUrl = buildUrl(ogUrl)
   return (
     <Helmet>
       {/* Meta tags de base */}
@@ -17,17 +32,33 @@ const SEO = ({
       
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={ogUrl} />
+      <meta property="og:url" content={finalUrl} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={ogImage} />
+      <meta property="og:site_name" content="Evo E-com" />
       
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={ogUrl} />
+      <meta property="twitter:url" content={finalUrl} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={ogImage} />
+      
+      {/* Meta tags pour forcer l'ouverture dans le navigateur mobile */}
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="format-detection" content="telephone=no" />
+      
+      {/* Meta tags pour éviter l'ouverture dans les apps sociales */}
+      <meta name="twitter:app:name:iphone" content="Safari" />
+      <meta name="twitter:app:name:ipad" content="Safari" />
+      <meta name="twitter:app:name:googleplay" content="Chrome" />
+      
+      {/* Meta tags pour Instagram et autres réseaux */}
+      <meta property="al:web:url" content={finalUrl} />
+      <meta property="al:web:should_fallback" content="true" />
       
       {/* Autres meta tags */}
       <meta name="robots" content="index, follow" />
